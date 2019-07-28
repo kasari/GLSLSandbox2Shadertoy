82581852f36d61c434f4066c36dd7f0c304adbd6
kasari <kasari3939@gmail.com>
2019-07-28 20:39:17 +0900

    Fix replace logic of main function

--- src/main.js
+++ src/main.js
@@ -13,36 +13,31 @@ ShadertoyEditor.setReadOnly(true);
 GLSLSandboxEditor.session.on('change', (delta) => {
   const src = GLSLSandboxEditor.getValue()
   const lines = src.split('\n')
-  
+
   // uniform 文の削除
   let newLines = lines.filter(l => !l.includes("uniform"))
-  
-  // main 関数の置換
-  newLines = newLines.map(l => {
-    if (l.includes("main")) return "void mainImage( out vec4 fragColor, in vec2 fragCoord ) {"
-    return l
-  })
-  
+
   let dst = newLines.join('\n')
-  
+
+  // main 関数の置換
+  dst = dst.replace(/void\s+main.+\)/g, "void mainImage( out vec4 fragColor, in vec2 fragCoord )")
+
   // GLSLSandbox: uniform vec2 resolution;
   // Shadertoy:   uniform vec3 iResolution;
   // GLSLSandbox と Shardertoy で型が異なるため、一度明示的な実装に置き換えた後に変数名を置換する
   dst = dst.replace(/resolution([^\.])/g, "resolution.xy$1")
   dst = dst.replace(/resolution/g, "iResolution")
-  
+
   // GLSLSandbox: uniform vec2 mouse;
   // Shadertoy:   uniform vec4 iMouse;
   dst = dst.replace(/mouse([^\.])/g, "mouse.xy$1")
   dst = dst.replace(/mouse/g, "iMouse")
-  
+
   dst = dst.replace(/time/g, "iTime")
-  
+
   // gl_FragCoord -> fragCoord
   // gl_FragColor -> fragColor
   dst = dst.replace(/gl_F/g, "f")
-  
+
   ShadertoyEditor.setValue(dst);
 })
-
-
